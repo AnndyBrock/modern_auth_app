dc25b21562e35d1f435e9016b38ce4d9ce84ede3
atroian <andrii.troian@corezoid.com>
2024-12-11 00:29:56 -0500

    added removed session endpoint

--- backend/src/controllers/session.controlle.ts
+++ backend/src/controllers/session.controlle.ts
@@ -1,6 +1,8 @@
 import catchErrors from "../utils/catchErrors";
-import { OK } from "../constants/http";
+import { NOT_FOUND, OK } from "../constants/http";
 import SessionModel from "../models/session.model";
+import { z } from "zod";
+import appAssert from "../utils/appAssert";
 
 export const getSessionHandler = catchErrors(async (req, res) => {
     const userId = res.locals?.userId;
@@ -16,4 +18,18 @@ export const getSessionHandler = catchErrors(async (req, res) => {
     res.status(OK).json(sessions.map(x => ({
         ...x.toObject(), ...(x.id === sessionId && { isCurrent: true })
     })));
+});
+
+export const deleteSessionHandler = catchErrors(async (req, res) => {
+    const sessionId = z.string().parse(req.params.id);
+    const userId = res.locals?.userId;
+
+    const deleted = await SessionModel.findOneAndDelete({
+        _id: sessionId,
+        userId
+    });
+
+    appAssert(deleted, NOT_FOUND, "Session not found");
+
+    return res.status(OK).json({ message: "Session removed" });
 });
--- backend/src/routes/sessions.route.ts
+++ backend/src/routes/sessions.route.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import {
     getSessionHandler,
+    deleteSessionHandler
 } from "../controllers/session.controlle";
 
 const authRoute = Router();
 
 authRoute.get("/", getSessionHandler);
+authRoute.delete("/:id", deleteSessionHandler);
 
 export default authRoute;
